0f355ee7728ee4bb2e910264783783574d38582b
Davi-prussek <davi.mariano.prussek0916@gmail.com>
2026-05-06 17:06:16 -0300

    Correção da permissão

--- django_project/settings.py
+++ django_project/settings.py
@@ -68,12 +68,6 @@ TEMPLATES = [
     },
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
-
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
