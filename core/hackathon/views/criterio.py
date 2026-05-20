a04390d1bd0dbd16fa9140cf38e0ec4993b36319
pedrojoseroza <pedrojs.ol.daroza@gmail.com>
2026-05-20 07:50:41 -0300

    FEAT: Adição das validações para somente admin poder adicionar critérios #10

--- core/hackathon/views/criterio.py
+++ core/hackathon/views/criterio.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-
+from rest_framework.permissions import IsAdminUser
 from ..models import Criterio
 from ..serializers import CriterioSerializer
 
 class CriterioViewSet(ModelViewSet):
     queryset = Criterio.objects.all()
     serializer_class = CriterioSerializer
+    # Validação de 
+    permission_classes = [IsAdminUser]
