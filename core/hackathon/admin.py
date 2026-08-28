542fa13decd7a64794de8662bcc89e4e58d698fe
MatheusZenere <matheus.vidal.zenere@gmail.com>
2026-08-28 14:27:24 -0300

    chore#21-Adicionando caminho do users no admin

--- core/hackathon/admin.py
+++ core/hackathon/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Apoiador, TipoEdicao, TipoUser, Edicao, Criterio, Tema, Projeto, Equipe, Nota
+from .models import Apoiador, TipoEdicao, TipoUser, User, Edicao, Criterio, Tema, Projeto, Equipe, Nota
 
 admin.site.register(Apoiador)
 admin.site.register(TipoEdicao)
 admin.site.register(TipoUser)
+admin.site.register(User)
 admin.site.register(Edicao)
 admin.site.register(Criterio)
 admin.site.register(Tema)
