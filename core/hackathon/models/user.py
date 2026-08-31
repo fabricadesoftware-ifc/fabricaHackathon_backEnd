70ffbb5ec88c4be7961641123c41b04230326b71
renan1811 <rennlima477@gmail.com>
2026-08-31 08:48:30 -0300

    feat#21/Arrumando o protect de auth_user dentro da model user.py

--- core/hackathon/models/user.py
+++ core/hackathon/models/user.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User as AuthUser
 from .tipo_user import TipoUser
 
 class User(models.Model):
-    auth_user = models.OneToOneField(AuthUser, on_delete=models.CASCADE, related_name='hackathon_user')
+    auth_user = models.OneToOneField(AuthUser, on_delete=models.PROTECT, related_name='hackathon_user')
     nome_user = models.CharField(max_length=255, null=False, blank=False)
     email_user = models.EmailField(unique=True, null=False, blank=False)
     tipoUser = models.ForeignKey(TipoUser, on_delete=models.PROTECT, null=False, blank=False)
