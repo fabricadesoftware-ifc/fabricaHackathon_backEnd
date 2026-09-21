4ada9dbebc06e9404be595122a791fe70ba89b0b
MatheusZenere <matheus.vidal.zenere@gmail.com>
2026-09-21 13:19:20 -0300

    CHORE-24: model avaliadorEdicao

--- core/hackathon/models/avaliadorEdicao.py
+++ core/hackathon/models/avaliadorEdicao.py
@@ -1,54 +1,14 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-from .user import User, tipoUser
 from .edicao import Edicao
+from .user import User
 
 
 class AvaliadorEdicao(models.Model):
-    user = models.ForeignKey(
-        User,
-        on_delete=models.PROTECT,
-        related_name="edicoes_avaliador",
-        blank=False,
-        null=False
-    )
-    edicao = models.ForeignKey(
-        Edicao,
-        on_delete=models.PROTECT,
-        related_name="avaliadores",
-        blank=False,
-        null=False
-    )
-
-    def clean(self):
-        if self.user_id and self.user.tipoUser != tipoUser.avaliador:
-            raise ValidationError({
-                "user": "Apenas usuários com perfil de Avaliador podem ser vinculados a uma edição."
-            })
-
-        if self.user_id and self.edicao_id:
-            qs = AvaliadorEdicao.objects.filter(
-                user_id=self.user_id,
-                edicao_id=self.edicao_id
-            )
-            if self.pk:
-                qs = qs.exclude(pk=self.pk)
-            if qs.exists():
-                raise ValidationError({
-                    "user": "Este usuário já está vinculado como avaliador nesta edição."
-                })
-
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
+    edicao = models.ForeignKey(Edicao, on_delete=models.PROTECT, related_name="avaliadores")
+    avaliador = models.ForeignKey(User, on_delete=models.PROTECT, related_name="edicoes_avaliadas")
 
     def __str__(self):
-        return f"{self.user} - {self.edicao}"
+        return f"{self.avaliador.get_full_name()} - {self.edicao.nome}"
 
     class Meta:
-        verbose_name = "Avaliador da Edição"
-        verbose_name_plural = "Avaliadores da Edição"
-        unique_together = ('user', 'edicao')
-
-
-Avaliador = AvaliadorEdicao
+        verbose_name_plural = "Avaliadores por Edição"
